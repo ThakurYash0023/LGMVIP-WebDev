650425ef384b2a9eb98233a78cc969dd8459f181
Yash <yashofficial0023@gmail.com>
2021-07-29 10:54:03 +0530

    putting Task2 for LGMVIP internship

--- Task-2/src/MyComponents/Loader.js
+++
@@ -1,7 +0,0 @@
-const Loader = ({ load }) => {
-    return (
-        (load) ?
-            <div className="loader_container"> <div className="loaderCircle"></div></div> : ""
-    )
-}
-export default Loader;
